79510083dff202d3f105afe2188188780c707b69
Hossam-Soliman <60302905+Hossam-Soliman@users.noreply.github.com>
2020-04-20 06:34:21 +0300

    Add files via upload

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -1,85 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {connect} from 'react-redux'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 
+const IndexPage = ({newUser}) => {                                   //I've passed the newUser coming from redux store as props
 
-class IndexPage extends Component {
+    const [countries, setCountries] = useState([]);
 
-    state = {
-        countries:[]
-    }
-   
+    useEffect (() => {
 
-    componentDidMount(){
         axios.post('https://countries.trevorblades.com/', { 
-        query: `
-            query{
-                countries{
-                    name
-                    code
-                    emoji
+            query: `
+                query{
+                    countries{
+                        name
+                        code
+                        emoji
+                    }
+                }`
 
-                }
-            }`
-        }
+            }).then(res =>{
+                setCountries(res.data.data.countries)
+            }) 
+    })
 
-        ).then(res =>{
-            console.log(res.data.data)
-            this.setState({
-                countries: res.data.data.countries
-            })
-        })       
-    }
-
-   render(){
-       console.log(this.props.newUser)
-       const user = this.props.newUser
-
-       const countriesList = this.state.countries.map(country =>{
-           return(
-                <tbody key={country.code}>
-                    <tr>
-                        <td>{country.code}</td>
-                        <td>{country.name}</td>
-                        <td><img src= {`https://www.countryflags.io/${country.code}/flat/32.png`} alt=""></img></td>
-                        <Link to ={`/show/${country.code}`}><td><i className="material-icons">visibility</i></td></Link>
-                    </tr>
-                </tbody>
-           )
-       })
-        
+    const countriesList = countries.map(country =>{
         return(
-            <div className="IndexPage">  
-                <div className= "user-page">User page</div>
-                    <div className = "userData">
-                        <span>user email: {user.email} </span><br></br>
-                        <span>user password: {user.password}</span>
-                    </div>
-
-                    <div className="table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>code</th>
-                                    <th>name</th>
-                                    <th>emoji</th>
-                                    <th>action</th>
-                                </tr>
-                            </thead>
-                            {countriesList}
-                        </table>
-                    </div>
-            </div>  
+             <tbody key={country.code}>
+                 <tr>
+                     <td>{country.code}</td>
+                     <td>{country.name}</td>
+                     <td><img src= {`https://www.countryflags.io/${country.code}/flat/32.png`} alt=""></img></td>
+                     <Link to ={`/show/${country.code}`}><td><i className="material-icons">visibility</i></td></Link>
+                 </tr>
+             </tbody>
         )
-   }    
-}
+    })
 
+    return (  
+        <div className="IndexPage">  
+            <div className= "user-page">User page</div>
+                <div className = "userData">
+                    <span>user email: {newUser.email} </span><br></br>
+                    <span>user password: {newUser.password}</span>
+                </div>
+
+                <div className="table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>code</th>
+                                <th>name</th>
+                                <th>emoji</th>
+                                <th>action</th>
+                            </tr>
+                        </thead>
+                        {countriesList}
+                    </table>
+                </div>
+        </div> 
+
+    );
+}
+ 
 const mapStateToProps = (state) =>{
-    return{
-        newUser: state
-    }
+    return { newUser: state }
 }
 
-export default connect(mapStateToProps)(IndexPage)
-
+export default connect(mapStateToProps)(IndexPage);
